c96e42f8dda4555af3e78b75447517e2b3876b77
MrActividades854 <kevinjoelescobar2020@gmail.com>
2026-04-21 23:33:29 -0500

    viewport

--- core/state.js
+++ core/state.js
@@ -1,7 +1,13 @@
 export const state = {
     elements: [],
     selected: null,
+
     tool: "select", // select | move | rotate | resize
     handles: null, // resize | rotate 
     handleDirection: null, // tl, tr, bl, br
+    
+    canvasZoom: 1,
+    canvasOffsetX: 0,
+    canvasOffsetY: 0,
+    isPanning: false,
   };
